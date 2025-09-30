3966e8d886f9d66bfc6a5d3bf5488b4a3af0da21
touraye <touraye07@gmail.com>
2025-09-30 19:02:33 +0000

    update: add module

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Contact from "@/components/home/contact";
 // import FeaturedRoles from "@/components/home/featured-roles";
 import Hero from "@/components/home/hero";
 import Publications from "@/components/home/publications";
+import FeaturedRolesSection from "@/components/home/roles/featured-roles";
 import Newsletter from "@/components/shared/news-letter";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
     
     {/* FeaturedRoles section */}
     {/* <FeaturedRoles /> */}
+    <FeaturedRolesSection />
 
     {/* Contact section */}
     <Contact />
