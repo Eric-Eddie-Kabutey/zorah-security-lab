c765cb3328ea5a3f765e794ad035569e14d554a6
touraye <touraye07@gmail.com>
2025-09-30 18:37:27 +0000

    update: comment out unused module path

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Contact from "@/components/home/contact";
-import FeaturedRoles from "@/components/home/featured-roles";
+// import FeaturedRoles from "@/components/home/featured-roles";
 import Hero from "@/components/home/hero";
-import Publications from "@/components/home/publications";
+// import Publications from "@/components/home/publications";
 import Newsletter from "@/components/shared/news-letter";
 
 export default function Home() {
