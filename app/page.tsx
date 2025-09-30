40e1541ca1868ad219552087a6c79e85dc4ef825
touraye <touraye07@gmail.com>
2025-09-30 18:50:10 +0000

    update: add publications

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Contact from "@/components/home/contact";
 // import FeaturedRoles from "@/components/home/featured-roles";
 import Hero from "@/components/home/hero";
-// import Publications from "@/components/home/publications";
+import Publications from "@/components/home/publications";
 import Newsletter from "@/components/shared/news-letter";
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
     <Hero />
 
     {/* Publications section */}
-    {/* <Publications /> */}
+    <Publications />
     
     {/* FeaturedRoles section */}
     {/* <FeaturedRoles /> */}
