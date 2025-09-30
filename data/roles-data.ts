ab60c0a9fa63ddfba2144b985d7a9834d8b4b9eb
touraye <touraye07@gmail.com>
2025-09-30 19:01:30 +0000

    add data - roles

---
+++ data/roles-data.ts
@@ -0,0 +1,8 @@
+// The data for the featured roles
+export const roles = [
+    { title: "AI Researcher", href: "/careers/ai-researcher" },
+    { title: "Cyber Researcher", href: "/careers/cyber-researcher" },
+    { title: "Research Engineer", href: "/careers/research-engineer" },
+    { title: "Technical Policy Researcher", href: "/careers/technical-policy-researcher" },
+    { title: "The Role You Are Perfect For", href: "/careers/general-application" },
+];
