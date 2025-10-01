3d906da4145262d881e483a2e426da2555588ecb
touraye <touraye07@gmail.com>
2025-10-01 16:41:56 +0000

    add data - hiring process

---
+++ data/hiringprocess-data.ts
@@ -0,0 +1,39 @@
+// Data for the hiring process steps
+export const processSteps = [
+  {
+    title: 'Job Application',
+    description: 'CV and additional information submitted in the form under the specific job.',
+  },
+  {
+    title: 'Brief Exploratory Chat',
+    description: 'A 15-minute online chat with one of our team members to discuss your interests and experience and for candidates to learn more about Irregular.',
+  },
+  {
+    title: 'Take-Home Assignment',
+    description: 'Assignments usually take ±5 hours, depending on the role.',
+  },
+  {
+    title: 'In-Depth Exploratory Chat',
+    description: 'A deeper, face-to-face conversation with a senior team member.',
+  },
+  {
+    title: 'Interviews Day',
+    description: '2-3 one hour to an hour and a half technical interviews. Plus, an organizational fit interview with a member of our leadership. Often, the technical interviews and fit interviews will be done on different dates to accommodate mutual scheduling needs. These interviews are usually done at our office.',
+  },
+  {
+    title: 'References',
+    description: 'We typically ask for 3 references from different organizations / roles, but depending on circumstances 2 references could suffice.',
+  },
+  {
+    title: 'Offer',
+    description: 'We’ll contact prospective candidates with an offer.',
+  },
+];
+
+// Data for the notes section
+export const notes = [
+  'We included multiple steps to inform our hiring decisions better (even though, as mentioned, not all people go through all of the steps). We recognize that going through the process requires time and effort; nonetheless, we think that using a variety of tools (tests, interviews, reference checks, etc.) is essential to reduce biases and help you showcase your strengths. In this spirit, we will do our best to accommodate requests to set each stage at a convenient time.',
+  'As mentioned above, what we describe is our standard process. Depending on the role and circumstances, we may decide to add a stage or skip multiple stages. We deeply value your time – that’s why we are firmly committed to not adding more than one additional stage beyond what’s outlined above.',
+  'While we have several steps in our process, we can move things along swiftly! If you’re working with a specific timeline, just let us know as soon as possible. We’ll do our best to make it work for you.',
+  'To make the process maximally useful to you, you should feel very comfortable asking us questions about Irregular in any conversation, and we plan the interviews to include enough time for you to do that.',
+];
