588aa5c85178229d8d5fa458a2c92f477ffe90c1
touraye <touraye07@gmail.com>
2025-10-01 16:38:30 +0000

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+import AboutContent from '@/components/about/about-content'
+import Newsletter from '@/components/shared/news-letter'
+import PageHeader from '@/components/shared/page-header'
+
+export default function AboutPage() {
+	return (
+		<>
+			{/* Hero section */}
+			<div className='grid grid-cols-1 lg:grid-cols-5'>
+				{/* empty space */}
+				<div className='hidden lg:block lg:col-span-2'></div>
+
+				<div className='lg:col-span-3 max-w-[572px] mx-auto md:-ml-20'>
+					<PageHeader title='About Zorah Security Lab' />
+					{/* About content section */}
+					<AboutContent />
+				</div>
+			</div>
+
+			{/* Newsletter section */}
+			<Newsletter />
+		</>
+	)
+}
