e2db603dc0c7317db0f70772aef7d7d32e9a74c1
touraye <touraye07@gmail.com>
2025-10-01 16:39:43 +0000

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/contact/contact-form";
+import Newsletter from "@/components/shared/news-letter";
+
+export default function ContactPage() {
+    return (<>
+        {/* Contact form  */}
+        <ContactForm />
+
+        {/* Newsletter section */}
+        <Newsletter />
+    </>)
+}
