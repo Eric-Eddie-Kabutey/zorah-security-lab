bd61bfa7ec8e906e3258d79353b7973dec43d460
touraye <touraye07@gmail.com>
2025-10-02 13:00:38 +0000

    update: add divider

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/components/home/contact";
 import Hero from "@/components/home/hero";
 import Publications from "@/components/home/publications";
 import FeaturedRolesSection from "@/components/home/roles/featured-roles";
+import Divider from "@/components/shared/divider";
 import Newsletter from "@/components/shared/news-letter";
 
 export default function Home() {
@@ -10,13 +11,21 @@ export default function Home() {
     {/* Hero section */}   
     <Hero />
 
+    {/* Divider section */}
+    <Divider />
+
     {/* Publications section */}
     <Publications />
+
+    {/* Divider section */}
+    <Divider />
     
     {/* FeaturedRoles section */}
     {/* <FeaturedRoles /> */}
     <FeaturedRolesSection visible={true} />
 
+    {/* Divider section */}
+    <Divider />
     {/* Contact section */}
     <Contact />
 
