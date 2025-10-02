9d8ec3481f0a2284e3b8b7434d1aa3c9d4c20e3b
touraye <touraye07@gmail.com>
2025-10-02 18:33:05 +0000

    update: mega menu links

--- components/layout/header/mega-menu-links.tsx
+++ components/layout/header/mega-menu-links.tsx
@@ -14,11 +14,7 @@ const MegaMenuLinkItem: React.FC<MegaMenuLinkItemProps> = ({ link }) => {
 			<p className='font-semibold text-foreground group-hover:text-foreground/70 transition-colors'>
 				{link.title}
 			</p>
-			{link.description?.map((item, index) => (
-				<p key={index} className='text-sm text-foreground/70'>
-					{item}
-				</p>
-			))}
+            {link.hasBnt && <button className='bg-gray-200 text-black text-sm'>{ link.title}</button>}
 		</Link>
 	)
 }
