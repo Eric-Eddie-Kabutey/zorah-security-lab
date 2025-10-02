1a6cdf602385553f49a8c370c33955610f3843c9
touraye <touraye07@gmail.com>
2025-10-02 17:43:26 +0000

    update: types

--- components/layout/header/mega-menu-links.tsx
+++ components/layout/header/mega-menu-links.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import Link from 'next/link';
-import { MegaMenuLink } from '@/types/nav-types';
+import React from 'react'
+import Link from 'next/link'
+import { MegaMenuLink } from '@/types/nav-types'
 
 interface MegaMenuLinkItemProps {
-  link: MegaMenuLink;
+	link: MegaMenuLink
 }
 
 const MegaMenuLinkItem: React.FC<MegaMenuLinkItemProps> = ({ link }) => {
-  return (
-    <Link href={link.href} className="group block">
-      <p className="font-semibold text-foreground group-hover:text-foreground/70 transition-colors">
-        {link.title}
-      </p>
-      <p className="text-sm text-foreground/70">
-        {link.description}
-      </p>
-    </Link>
-  );
-};
+    console.log("link", link);
+    
+	return (
+		<Link href={link.href} className='group block'>
+			<p className='font-semibold text-foreground group-hover:text-foreground/70 transition-colors'>
+				{link.title}
+			</p>
+			{link.description?.map((item, index) => (
+				<p key={index} className='text-sm text-foreground/70'>
+					{item}
+				</p>
+			))}
+		</Link>
+	)
+}
 
-export default MegaMenuLinkItem;
+export default MegaMenuLinkItem
