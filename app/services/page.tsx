070d49d5fcb948d3a6a76bfa1ac3ea3a154495dd
touraye <touraye07@gmail.com>
2025-10-02 12:42:19 +0000

    update: add divider

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,17 +1,22 @@
 import Hero from '@/components/services/hero'
 import ServicesGrid from '@/components/services/service-grid'
 import ServicesListing from '@/components/services/services-listing'
+import Divider from '@/components/shared/divider'
 
 export default function AboutPage() {
     return (<>
         {/* Hero section*/}
         <Hero />
 
-        {/* Content section */}
-        
+        {/* Divider section */}
+        <Divider />
 
         {/* Service list */}
         <ServicesListing />
+
+        {/* Divider section */}
+        <Divider />
+
 
         {/* Services cards */}
         <ServicesGrid />
