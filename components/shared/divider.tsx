d0cf78d938d03e11b5f0c4bcac87adfd616ac4f4
touraye <touraye07@gmail.com>
2025-10-02 12:40:36 +0000

    add divider

---
+++ components/shared/divider.tsx
@@ -0,0 +1,11 @@
+import { WaveDivider } from "../svg/wave-divider"
+
+const Divider: React.FC = () => {
+    return (
+        <section className="max-w-[1230px] 2xl:max-w-[1390px] mx-auto px-6">
+            <WaveDivider />            
+        </section>
+    )
+}
+
+export default Divider;
