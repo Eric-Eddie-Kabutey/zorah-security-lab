1d8bf20295e963184f1cd203f9bfe9e27f89ad43
touraye <touraye07@gmail.com>
2025-10-02 12:41:45 +0000

    update: add heading and description to hero - add content tile to services listings

--- components/services/services-listing.tsx
+++ components/services/services-listing.tsx
@@ -5,15 +5,27 @@ import React from 'react';
 const ServicesListing: React.FC = () => {
   return (
     <section id="hiring-process" className="bg-background py-24 px-6">
-      <div className="max-w-[1230px] 2xl:max-w-[1390px] mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
         {/* Left Column: Section Title */}
-              <div>
-                  <p className='text-gray-800 text-sm md:text-base sticky top-28'>Services</p>
-          <h2 className="text-lg font-medium text-black sticky top-36">
+              <div className='md:sticky md:top-40'>
+                  <div className='md:sticky md:top-38 mb-10'>
+                  <p className='text-gray-800 text-sm md:text-base mb-2'>Services</p>
+          <h2 className="text-2xl md:text-3xl font-medium text-black mb-4">
             Our Core Service
                   </h2>
                   {/* underline  */}
-                  <div className='bg-gray-500 h-1 w-10 sticky top-46'></div>
+                  <div className='bg-gray-400 h-1 w-10'></div>
+                      
+                  </div>
+
+                  {/* table of content */}
+                  <ul className='list-none md:sticky top-64 cursor-pointer'>
+                      <li className='text-black font-normal md:font-medium text-[16px]  mb-2'>Job Application</li>
+                      <li className='text-black font-normal md:font-medium text-[16px]  mb-2'>Brief Exploratory Chat</li>
+                      <li className='text-black font-normal md:font-medium text-[16px]  mb-2'>Take-Home Assignment</li>
+                      <li className='text-black font-normal md:font-medium text-[16px]  mb-2'>In-Depth Exploratory Chat</li>
+                      <li className='text-black font-normal md:font-medium text-[16px]  mb-2'>Interviews Day</li>
+                  </ul>
         </div>
 
         {/* Right Column: Main Content */}
