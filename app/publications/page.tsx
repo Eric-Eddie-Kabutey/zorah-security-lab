94301e9d88a4c3c078c16c4a5b611bdc6cfdbfd1
touraye <touraye07@gmail.com>
2025-10-03 18:36:00 +0000

    update: getAllPublications now return and array

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -3,14 +3,9 @@ import Newsletter from "@/components/shared/news-letter";
 import PageHeader from "@/components/shared/page-header";
 import { getAllPublications } from "@/lib/mdx";
 
-export default function PublicationsPage() {
-    const allPosts = getAllPublications();
-  
-  const publicationsFrontmatter = allPosts.map(post => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { content, ...frontmatter } = post;
-    return frontmatter;
-  });
+export default function PublicationsPage() {  
+  const publicationsFrontmatter = getAllPublications();
+   
     return (<>
         {/* Hero section */}        
         <PageHeader title="Publications" />        
