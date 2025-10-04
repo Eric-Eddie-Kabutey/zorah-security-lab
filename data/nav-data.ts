0077f6df16469556b6485dbe0cbef87aafaace88
touraye <touraye07@gmail.com>
2025-10-04 09:18:48 +0000

    update data - nav

--- data/nav-data.ts
+++ data/nav-data.ts
@@ -1,5 +1,4 @@
 import { NavItem } from '@/types/nav-types'
-// Import other icons...
 
 export const navigationItems: NavItem[] = [
     {
@@ -13,115 +12,30 @@ export const navigationItems: NavItem[] = [
     {
         label: 'Services',
         href: '#',
-        megaMenuContent: [
+        dropdownMenuContent: [
             {
                 title: 'Digital Forensics',    
+                href: '/services/digital-forensics',
                 description: 'Some description coming some for digital forensics',
-                linkLayout: 'list',
-                links: [
-                    {
-                        title: 'Computer Forensics',
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/digital-forensics#computerforensics',
-                    },
-                    {
-                        title: 'Mobile Forensics',   
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/digital-forensics#mobileforensics',
-                    },
-                    {
-                        title: 'ECU Forensics',  
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/digital-forensics#ecuforensics',
-                    },  
-                    {
-                        title: 'Drone Forensics',
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/digital-forensics#drone-forensics',
-                    },  
-                ],
+                iconUrl: '/assets/icons/digital-forensics/link-icon.png'
             },
             {
                 title: 'Cyber Enable Crime',  
-                description: 'Some description coming some for cyber enable crime',
-                linkLayout: 'list',
-                links: [
-                    {
-                        title: 'BEC Investigation',    
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/cyber-enable-crime#bec-investigations',
-                    },
-                    {
-                        title: 'Penetration Testing',  
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/cyber-enable-crime#penetration-testing',
-                    },
-                    {
-                        title: 'Data Breach Investigation', 
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/cyber-enable-crime#data-breach-investigation',
-                    },
-                    {
-                        title: 'Malware (Viral) Remote',  
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/cyber-enable-crime#malware-viral-remote',
-                    },
-                ],
+                href: '/services/cyber-enable-crime',
+                description: 'Some description coming some for cyber enable crime',   
+                iconUrl: '/assets/icons/cyber-enable-crime/link-icon.png'
             },            
             {
                 title: 'Network Intelligence',     
+                href: '/services/network-intelligence',
                 description: 'Some description coming some for network intelligence',
-                linkLayout: 'list',
-                links: [
-                    {
-                        title: 'Ethical Hacking Team',  
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/network-intelligence#ethical-hacking',
-                    },
-                    {
-                        title: 'WIFI Probe',      
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/network-intelligence#wifi-probe',
-                    },
-                    {
-                        title: 'RSL Hacking Van',
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/network-intelligence#rls-hacking-van',
-                    },
-                    {
-                        title: 'GSM Jamming Detector',
-                        imgUrl: '/assets/icons/setting.png',
-                        href: '/network-intelligence#gms-jamming-detector',
-                    },
-                ],
+                iconUrl: '/assets/icons/network-intelligence/link-icon.png'
             },
             {
                 title: 'Automotive Investigation',
+                href: '/services/automotive-investigation',
                 description: 'Some description coming some for automotive investigation',
-                linkLayout: 'list',
-                links: [
-                    {
-                        title: 'Stolen Vehicle Investigation',
-                        imgUrl: '/assets/icons/setting.png',                        
-                        href: '/automotive-investigation#stolenvehicleinvestigation',
-                    },
-                    {
-                        title: 'Rental Car Theft',
-                        imgUrl: '/assets/icons/setting.png',                        
-                        href: '/automotive-investigation#rentalcartheft',
-                    },
-                    {
-                        title: 'OEM Terematics',
-                        imgUrl: '/assets/icons/setting.png',                        
-                        href: '/automotive-investigation#oem',
-                    },
-                    {
-                        title: 'Patrol Emergency and Intelligent',
-                        imgUrl: '/assets/icons/setting.png',                        
-                        href: '/automotive-investigation#patrolemergencyintelligent',
-                        hasBnt: true
-                    },
-                ],
+                iconUrl: '/assets/icons/automotive-investigation/link-icon.png'
             },
         ],
     },
