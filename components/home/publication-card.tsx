5a1cc2fca2e7c47e3119cb31935ebceb705f3816
touraye <touraye07@gmail.com>
2025-10-06 11:34:20 +0000

    update: remove unused modules

--- components/home/publication-card.tsx
+++ components/home/publication-card.tsx
@@ -2,19 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { motion, Variants } from 'framer-motion';
 import { Publication } from '@/data/publication-data';
-
-// A simple placeholder icon component
-const AbstractIcon = () => (
-  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="20" cy="20" r="20" fill="#E0F2FE" />
-    <path d="M15 25L25 15" stroke="#0284C7" strokeWidth="2" strokeLinecap="round"/>
-    <path d="M15 15L25 25" stroke="#0EA5E9" strokeWidth="2" strokeLinecap="round"/>
-  </svg>
-);
-
-
 
 interface PublicationCardProps {
   publication: Publication;  
